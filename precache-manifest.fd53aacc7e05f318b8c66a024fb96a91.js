87764ca0ef8d1341a2cf2d7e801f9a1cba1bf0d7
maciekz1996 <maciek3609@gmail.com>
2020-09-09 20:59:45 +0200

    Updates

---
+++ precache-manifest.fd53aacc7e05f318b8c66a024fb96a91.js
@@ -0,0 +1,74 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "30ba5a93bd5b062263ae27a6c70fa495",
+    "url": "/portfolio/index.html"
+  },
+  {
+    "revision": "cef04c722cfa11ab7421",
+    "url": "/portfolio/static/css/main.a7d42689.chunk.css"
+  },
+  {
+    "revision": "451f8d1b41ca127621d3",
+    "url": "/portfolio/static/js/2.b21eab96.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/portfolio/static/js/2.b21eab96.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "cef04c722cfa11ab7421",
+    "url": "/portfolio/static/js/main.979c8e69.chunk.js"
+  },
+  {
+    "revision": "dd124b4be1329afc536b",
+    "url": "/portfolio/static/js/runtime-main.314d17bb.js"
+  },
+  {
+    "revision": "460d9424c4aaa07f5c919543669b752d",
+    "url": "/portfolio/static/media/ballin.460d9424.png"
+  },
+  {
+    "revision": "87b0dd6159ec9cc59d1c69db910e3af8",
+    "url": "/portfolio/static/media/bg-contact-page.87b0dd61.svg"
+  },
+  {
+    "revision": "4f4919a305cd0c921b1a6405eeb2449b",
+    "url": "/portfolio/static/media/bg-mobile-projects-page.4f4919a3.svg"
+  },
+  {
+    "revision": "d0e5775d2b3ee58d2da7cd582d2e2e44",
+    "url": "/portfolio/static/media/bg-mobile-skills-page.d0e5775d.svg"
+  },
+  {
+    "revision": "182996c22c6e50f82b5842f1106cb4aa",
+    "url": "/portfolio/static/media/bg-projects-page.182996c2.svg"
+  },
+  {
+    "revision": "e5852b8c80c42e1654ff87428181b85b",
+    "url": "/portfolio/static/media/bg-skills-page.e5852b8c.svg"
+  },
+  {
+    "revision": "1d253773ae583b713653c7ebb1d3df18",
+    "url": "/portfolio/static/media/gymino-app.1d253773.png"
+  },
+  {
+    "revision": "15ec172c85fdbcde6ee396d9dca3306e",
+    "url": "/portfolio/static/media/gymino-dashboard.15ec172c.png"
+  },
+  {
+    "revision": "c7273b443e2a74515472fa65eae1f673",
+    "url": "/portfolio/static/media/instagram_clone.c7273b44.png"
+  },
+  {
+    "revision": "1f40d7752c817ca9622055dc94a6d649",
+    "url": "/portfolio/static/media/polskalokalna.1f40d775.png"
+  },
+  {
+    "revision": "405212787dc0f26909d1ca0c80a321c5",
+    "url": "/portfolio/static/media/scudetto.40521278.png"
+  },
+  {
+    "revision": "653e8ad2399b3dba4c7dad6143cc5fb7",
+    "url": "/portfolio/static/media/sorting-visualizer.653e8ad2.png"
+  }
+]);
